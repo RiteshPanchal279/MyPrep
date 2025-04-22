4d295127b62fe6037ae1ef3759c2bb4df2a0f61b
RiteshPanchal279 <riteshpanchal279@gmail.com>
2025-04-22 22:13:21 +0530

    password eye icon add

--- components/FormField.tsx
+++ components/FormField.tsx
@@ -1,19 +1,20 @@
-import { Controller, Control, FieldValues, Path } from "react-hook-form";
-
+import React, { useState } from "react";
 import {
+  FormControl,
   FormItem,
   FormLabel,
-  FormControl,
   FormMessage,
 } from "@/components/ui/form";
 import { Input } from "@/components/ui/input";
+import { Control, Controller, FieldValues, Path } from "react-hook-form";
+import { FaRegEyeSlash, FaRegEye } from "react-icons/fa6";
 
 interface FormFieldProps<T extends FieldValues> {
   control: Control<T>;
   name: Path<T>;
   label: string;
   placeholder?: string;
-  type?: "text" | "email" | "password";
+  type?: "text" | "email" | "password" | "file";
 }
 
 const FormField = <T extends FieldValues>({
@@ -23,21 +24,35 @@ const FormField = <T extends FieldValues>({
   placeholder,
   type = "text",
 }: FormFieldProps<T>) => {
+  const [showPassword, setShowPassword] = useState(false);
+  const isPassword = type === "password";
+
   return (
     <Controller
-      control={control}
       name={name}
+      control={control}
       render={({ field }) => (
         <FormItem>
           <FormLabel className="label">{label}</FormLabel>
-          <FormControl>
-            <Input
-              className="input"
-              type={type}
-              placeholder={placeholder}
-              {...field}
-            />
-          </FormControl>
+          <div className="relative">
+            <FormControl>
+              <Input
+                className="input pr-10"
+                placeholder={placeholder}
+                type={isPassword ? (showPassword ? "text" : "password") : type}
+                {...field}
+              />
+            </FormControl>
+
+            {isPassword && (
+              <span
+                className="absolute right-3 top-1/2 -translate-y-1/2 text-gray-500 cursor-pointer"
+                onClick={() => setShowPassword((prev) => !prev)}
+              >
+                {showPassword ? <FaRegEyeSlash /> : <FaRegEye />}
+              </span>
+            )}
+          </div>
           <FormMessage />
         </FormItem>
       )}
